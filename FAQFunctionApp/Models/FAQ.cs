ea9fe034f96d0b660ad5719df62dbcb6f293cab5
jenry cacerez guzman <jenrybrown@hotmail.com>
2020-07-11 14:56:42 -0400

    Add project files.

---
+++ FAQFunctionApp/Models/FAQ.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FAQFunctionApp.Models
+{
+    class FAQ
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
